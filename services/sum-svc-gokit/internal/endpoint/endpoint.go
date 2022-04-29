a00f2d3a4435ed437db078dcfe2164f9406c5ec6
gabriel.camps <ext_gabcampsl@falabella.cl>
2022-04-29 09:58:13 -0500

    [UPD] endpoint

--- services/sum-svc-gokit/internal/endpoint/endpoint.go
+++ services/sum-svc-gokit/internal/endpoint/endpoint.go
@@ -2,6 +2,7 @@ package endpoint
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gclamigueiro/sum-svc-gokit/internal/entity"
 	"github.com/gclamigueiro/sum-svc-gokit/internal/service"
@@ -11,6 +12,7 @@ import (
 func MakeSumEndpoint(svc service.SumService) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
 		req := request.(entity.SumRequest)
+		fmt.Println("Sum Endpoint", req.A, req.B)
 		v := svc.Sum(req.A, req.B)
 		return entity.SumResponse{R: v, Err: ""}, nil
 	}
